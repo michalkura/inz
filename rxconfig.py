881664d769a9ad43f9b209b2c3888ad2a7760778
michalkura <kurowski.michal1@gmail.com>
2024-01-23 16:14:10 +0100

    vercel update

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="inzynierka",
-    api_url="inz-msh3-m4cn5ufaq-michalkuras-projects.vercel.app:8000"
+    api_url="cpmpert.vercel.app:8000"
 )
